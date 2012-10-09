d2d7cb0d10ea86f63595c4afcc4ebe72b91f5712
Florian Scheibner <flo.de@hotmail.de>
2012-10-09 12:47:55 +0200

    checkpoint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1470fe244455a273ed01af12f3e21feb838bd90f7b4573a7b0fec5521455dc5fadf70d07666c9028ffa97338b1dcd7c06c523303173477832236b6078eca700d'
